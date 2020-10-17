27c8cf0c155518edeb20dffda81f91baad6256e4
PBillingsby <philbillingsby@gmail.com>
2020-10-17 13:51:05 -0700

    Change git remote url

--- src/About.js
+++ src/About.js
@@ -17,19 +17,16 @@ const About = () => {
       </div>
       <br />
       <div className="mx-auto w-75">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum
-        commodi nam at illo iure, animi fugiat accusantium quia nesciunt debitis
-        quas, molestias ipsa qui dolor eaque, dolorem fuga iusto ullam? Eos, ad!
-        Facilis non soluta delectus, modi asperiores rerum a placeat, quasi
-        repellat blanditiis, maiores eos iure cupiditate saepe veniam aperiam
-        nam! Perspiciatis suscipit, nam deleniti odit eveniet alias repellat.
-        Quo tempore ad quas ducimus, qui quasi delectus cupiditate ipsam
-        obcaecati, nostrum minima, voluptatem praesentium. Dolorem eaque, ipsam,
-        animi ad temporibus odio qui amet nostrum quae laudantium, dignissimos
-        quasi asperiores. Ipsam blanditiis reprehenderit eius ab quasi
-        cupiditate porro dicta aut, itaque, corrupti maiores impedit culpa
-        corporis fugiat. Blanditiis porro iure quo necessitatibus, quas autem
-        fuga? Dicta, fuga. Quas, quam magni.
+        Ruby on Rails, React, JavaScript, Redux Facilis non soluta delectus,
+        modi asperiores rerum a placeat, quasi repellat blanditiis, maiores eos
+        iure cupiditate saepe veniam aperiam nam! Perspiciatis suscipit, nam
+        deleniti odit eveniet alias repellat. Quo tempore ad quas ducimus, qui
+        quasi delectus cupiditate ipsam obcaecati, nostrum minima, voluptatem
+        praesentium. Dolorem eaque, ipsam, animi ad temporibus odio qui amet
+        nostrum quae laudantium, dignissimos quasi asperiores. Ipsam blanditiis
+        reprehenderit eius ab quasi cupiditate porro dicta aut, itaque, corrupti
+        maiores impedit culpa corporis fugiat. Blanditiis porro iure quo
+        necessitatibus, quas autem fuga? Dicta, fuga. Quas, quam magni.
       </div>
     </div>
   );
