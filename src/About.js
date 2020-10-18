ffde7eadb691800c7b4c2f9d936daf917592a6d6
PBillingsby <philbillingsby@gmail.com>
2020-10-18 15:21:00 -0700

    Change about

--- src/About.js
+++ src/About.js
@@ -17,16 +17,16 @@ const About = () => {
       </div>
       <br />
       <div className="mx-auto w-75">
-        Ruby on Rails, React, JavaScript, Redux Facilis non soluta delectus,
-        modi asperiores rerum a placeat, quasi repellat blanditiis, maiores eos
-        iure cupiditate saepe veniam aperiam nam! Perspiciatis suscipit, nam
-        deleniti odit eveniet alias repellat. Quo tempore ad quas ducimus, qui
-        quasi delectus cupiditate ipsam obcaecati, nostrum minima, voluptatem
-        praesentium. Dolorem eaque, ipsam, animi ad temporibus odio qui amet
-        nostrum quae laudantium, dignissimos quasi asperiores. Ipsam blanditiis
-        reprehenderit eius ab quasi cupiditate porro dicta aut, itaque, corrupti
-        maiores impedit culpa corporis fugiat. Blanditiis porro iure quo
-        necessitatibus, quas autem fuga? Dicta, fuga. Quas, quam magni.
+        Ruby on Rails, React, JavaScript, Redux Facifasdfasdfdsalis non soluta
+        delectus, modi asperiores rerum a placeat, quasi repellat blanditiis,
+        maiores eos iure cupiditate saepe veniam aperiam nam! Perspiciatis
+        suscipit, nam deleniti odit eveniet alias repellat. Quo tempore ad quas
+        ducimus, qui quasi delectus cupiditate ipsam obcaecati, nostrum minima,
+        voluptatem praesentium. Dolorem eaque, ipsam, animi ad temporibus odio
+        qui amet nostrum quae laudantium, dignissimos quasi asperiores. Ipsam
+        blanditiis reprehenderit eius ab quasi cupiditate porro dicta aut,
+        itaque, corrupti maiores impedit culpa corporis fugiat. Blanditiis porro
+        iure quo necessitatibus, quas autem fuga? Dicta, fuga. Quas, quam magni.
       </div>
     </div>
   );
