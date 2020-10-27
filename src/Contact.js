0b7154e999388a326ce165e147af462e2533cbd9
PBillingsby <philbillingsby@gmail.com>
2020-10-27 12:38:50 -0700

    Add form to contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,42 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div className="container w-50 p-3 bg-light">
+        <form className="form-group row w-50">
+          <div className="col-sm">
+            <input
+              type="text"
+              name="name"
+              className="form-control mb-2"
+              placeholder="First Name"
+            />
+            <input
+              type="text"
+              name="lname"
+              className="form-control mb-2"
+              placeholder="Last Name"
+            />
+          </div>
+          <div className="pt-4">
+            <input
+              type="text"
+              name="email"
+              className="form-control mb-2"
+              placeholder="Email"
+            />
+          </div>
+          <input
+            type="text"
+            name="subject"
+            className="form-control"
+            placeholder="Subject"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
