844d60f6c13ddf971f72d62a55ca61bd86ea7c4a
PBillingsby <philbillingsby@gmail.com>
2020-11-02 14:24:41 -0800

    Add state to Work to show project on hover

--- src/Work.js
+++ src/Work.js
@@ -2,14 +2,40 @@ import React from "react";
 
 import ProjectCard from "./ProjectCard";
 import projects from "./Projects";
-const Work = () => {
-  return (
-    <div class="pt-4">
-      {projects.map(project => {
-        return <ProjectCard project={project} />;
-      })}
-    </div>
-  );
-};
+class Work extends React.Component {
+  state = {
+    project: null
+  };
+  showProject(projects, num) {
+    const shownProject = projects.find(project => project.index === num);
+    this.setState({
+      project: shownProject
+    });
+  }
+  removeProject() {
+    debugger;
+  }
+  render() {
+    return (
+      <div className="container row">
+        <div className="pt-5">
+          <ul>
+            <li
+              onMouseOver={() => this.showProject(projects, 2)}
+              onMouseOut={() => this.removeProject()}
+            >
+              Skate-Mate
+            </li>
+          </ul>
+        </div>
+        <div className="pt-4">
+          {this.state.project != null && (
+            <ProjectCard project={this.state.project} />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Work;
