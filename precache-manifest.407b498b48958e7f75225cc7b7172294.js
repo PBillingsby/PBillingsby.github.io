632d16601d712c1d1c7152e60041ad0ca837b4f4
PBillingsby <philbillingsby@gmail.com>
2020-11-02 15:31:41 -0800

    Updates

---
+++ precache-manifest.407b498b48958e7f75225cc7b7172294.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d322b279fba3ab5c0ceeac96b02f8667",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4e424be202e3327eb92",
+    "url": "/static/css/main.12bb9ba8.chunk.css"
+  },
+  {
+    "revision": "6c2bf876aed3a0c828bd",
+    "url": "/static/js/2.903b76cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.903b76cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4e424be202e3327eb92",
+    "url": "/static/js/main.1b690f8e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7019331cff03602079a7d6b864f5fe8b",
+    "url": "/static/media/ab-screenshot.7019331c.png"
+  },
+  {
+    "revision": "0b000d446032c7b93f752ee48a65cdb3",
+    "url": "/static/media/sm-screenshot.0b000d44.png"
+  },
+  {
+    "revision": "aff16240d15691ee98a25529522657c0",
+    "url": "/static/media/tdb-screenshot.aff16240.png"
+  }
+]);
