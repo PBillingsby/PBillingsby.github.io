33dc82d42c67808b3771d55d810cd292c62ffa3d
PBillingsby <philbillingsby@gmail.com>
2020-11-02 14:48:49 -0800

    Remove removeProject function to make sure project card always displayed after hover

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -1,14 +1,15 @@
 import React from "react";
+
 const ProjectCard = props => {
   const { project } = props;
 
   return (
-    <div className="p-3 mx-auto">
+    <div className="mx-auto">
       <div
         className="card mx-auto text-white bg-opacity"
         style={{ width: "50rem" }}
       >
-        <img src={project.src} className="card-img-top img-thumbnail" />
+        <img src={project.src} className="card-img-top" />
         <div className="card-body text-center">
           <h2 class="card-title">{project.title}</h2>
           <div class="card-info">
--- src/Work.js
+++ src/Work.js
@@ -12,39 +12,23 @@ class Work extends React.Component {
       project: shownProject
     });
   }
-  removeProject() {
-    if (this.state.project != null) {
-      this.setState({
-        project: null
-      });
-    }
-  }
   render() {
     return (
       <div className="container row">
         <div className="pt-5">
           <ul className="list-group">
-            <li
-              onMouseOver={() => this.showProject(projects, 2)}
-              onMouseOut={() => this.removeProject()}
-            >
+            <li onMouseOver={() => this.showProject(projects, 2)}>
               Skate-Mate
             </li>
-            <li
-              onMouseOver={() => this.showProject(projects, 1)}
-              onMouseOut={() => this.removeProject()}
-            >
+            <li onMouseOver={() => this.showProject(projects, 1)}>
               The Daily Bump
             </li>
-            <li
-              onMouseOver={() => this.showProject(projects, 0)}
-              onMouseOut={() => this.removeProject()}
-            >
+            <li onMouseOver={() => this.showProject(projects, 0)}>
               Arvo Beers
             </li>
           </ul>
         </div>
-        <div className="pt-4">
+        <div className="col-lg pt-2">
           {this.state.project != null && (
             <ProjectCard project={this.state.project} />
           )}
