5a7217dbdf497320e11e58e0a8fdf02dd07509f2
PBillingsby <philbillingsby@gmail.com>
2020-11-07 11:44:54 -0800

    Center footer links

--- src/Footer.js
+++ src/Footer.js
@@ -4,26 +4,30 @@ const Footer = () => {
   return (
     <div>
       <footer className="fixed-bottom pl-3 pb-2">
-        <p className="float-left">Phil Billingsby 2020</p>
-        <span className="float-left">
+        <span className="mx-auto">
+          <p className="d-inline-block">Phil Billingsby 2020</p>
+
           <a
             href="https://github.com/PBillingsby"
+            rel="noopener noreferrer"
             target="_blank"
-            className="p-2"
+            className="p-2 orange-color"
           >
             <i class="fa fa-github" aria-hidden="true"></i>
           </a>
           <a
             href="https://www.linkedin.com/in/phil-billingsby/"
+            rel="noopener noreferrer"
             target="_blank"
-            className="p-2"
+            className="p-2 orange-color"
           >
             <i class="fa fa-linkedin-square" aria-hidden="true"></i>
           </a>
           <a
             href="https://twitter.com/Phil_Billingsby"
+            rel="noopener noreferrer"
             target="_blank"
-            className="p-2"
+            className="p-2 orange-color"
           >
             <i class="fa fa-twitter-square" aria-hidden="true"></i>
           </a>
