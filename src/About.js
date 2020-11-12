f12fa1fe393fe0447859c65073297568274e5343
PBillingsby <philbillingsby@gmail.com>
2020-11-12 07:28:04 -0800

    Add grid for About

--- src/About.js
+++ src/About.js
@@ -2,19 +2,51 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="row">
-      <div className="float-left w-25 pt-4">
-        <h4 className="orange-color">Full-Stack Software Developer</h4>
-        <p>
-          Based in Las Vegas, Nevada. I enjoy designing, building, breaking, and
-          fixing web applications.
-        </p>
-      </div>
-      <br />
+    <div className="d-inline-flex">
       <div className="row">
-        <div className="col-sm">
-          <i className="fa fa-paint-brush mx-auto"></i>
-          <p className="text-center">Design</p>
+        <div className="float-left w-25 pt-4">
+          <h4 className="orange-color">Full-Stack Software Developer</h4>
+          <p>
+            Based in Las Vegas, Nevada. I enjoy designing, building, breaking,
+            and fixing web applications.
+          </p>
+        </div>
+        <div className="main mx-auto w-50">
+          <div className="row">
+            <div className="col-lg">
+              <h1>Lorem</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                ac elementum sem. Donec et pretium dolor. Cras tincidunt tempor
+                felis eu pellentesque. Duis at quam dolor. Pellentesque aliquet
+                tortor sed porttitor lacinia. Donec venenatis suscipit nibh in
+                tristique. Maecenas in fermentum lectus. Nam ut arcu tincidunt,
+                viverra purus nec, ullamcorper odio. Mauris aliquet sagittis
+                ipsum, sed imperdiet velit malesuada eget. Vivamus ac sodales
+                enim.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                ac elementum sem. Donec et pretium dolor. Cras tincidunt tempor
+                felis eu pellentesque. Duis at quam dolor. Pellentesque aliquet
+                tortor
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm">
+              <i className="fa fa-paint-brush"></i>
+              <p>Design</p>
+            </div>
+            <div className="col-sm">
+              <i className="fa fa-paint-brush"></i>
+              <p>Design</p>
+            </div>
+            <div className="col-sm">
+              <i className="fa fa-paint-brush"></i>
+              <p>Design</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
